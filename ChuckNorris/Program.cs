688af98f9818fb204edc3eb29181d2521c0902f5
Tanel Aavistu <tanel.aavistu@gmail.com>
2021-11-12 19:22:13 +0200

    first commit

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomQuote = GetRandomFromFile("chuck.txt");
+            Console.WriteLine(randomQuote);
+        }
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+        private static string GetRandomFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\...\samples\{fileName}";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+            return randomElement;
+        }
+    }
+}
---
+++ FridayNightRnd/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FridayNightRnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayRandomFood();
+            DisplayRandomDrink();
+            DisplayRandomMovie();
+        }
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+        private static void DisplayRandomFood()
+        {
+            string[] foods = { "chicken wings", "pizza", "sushi", "popcorn", "salad" };
+            Console.WriteLine($"Computer picked: {foods[GenerateRandomIndex(foods)]}.");
+        }
+        private static void DisplayRandomDrink()
+        {
+            string[] drinks = { "apple juice", "coke", "fanta", "sprite" };
+            Console.WriteLine($"Computer picked: {drinks[GenerateRandomIndex(drinks)]}.");
+        }
+        private static void DisplayRandomMovie()
+        {
+            string[] movies = { "The Wolf Of Wall Street", "Batman Begins", "The Joker", "The Budapest Hotel" };
+            Console.WriteLine($"Computer picked: {movies[GenerateRandomIndex(movies)]}.");
+        }
+        
+    }
+}
